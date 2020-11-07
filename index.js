2d84be6a9a80710aaae7fb1db71d5fbe1e52add2
bshseymur123 <54000319+bshseymur123@users.noreply.github.com>
2020-11-07 22:12:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const boyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 5000
 const MAX_NOTES = 100;
